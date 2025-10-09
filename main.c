de877af65dc834cdcf065630850a211a6ff6837c
hyna <bylo@duck.com>
2025-10-09 22:55:35 +0200

    feat(tp4/addition): automate addition fow, valide operation type given in console

--- main.c
+++ main.c
@@ -5,12 +5,34 @@
 int main(void)
 
 {
-    //  ceci est un commentaire
-    puts("***CALCULATRICE***");
-    float a = 2;
-    float b = 3;
-    float c = add(a, b);
 
-    printf("Result = %.2f\n", c);
+    int choice;
+    float a, b, result;
+
+    puts("*** CALCULATRICE ***");
+    puts("1 → Addition");
+    puts("2 → Soustraction");
+    puts("3 → Multiplication");
+    puts("4 → Division");
+
+    while (choice < 1 || choice > 4)
+    {
+        /* code */
+        printf("Choisissez une opération (1-4) : ");
+        scanf("%d", &choice);
+    }
+
+    printf("Entrez les deux nombres : ");
+    scanf("%f,%f", &a, &b);
+
+    switch (choice)
+    {
+    case 1:
+        /* code */
+        result = add(a, b);
+        break;
+    }
+
+    printf("Result = %.2f\n", result);
     return 0;
 }
